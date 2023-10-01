4b7fb855ac6725dda2baf640eca6815d9c3c2a0f
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-09-30 22:47:52 -0300

    prototype

--- objetos/constructor-function/exercicios/script.js
+++ objetos/constructor-function/exercicios/script.js
@@ -28,5 +28,17 @@ const bruno = new Pessoa('Bruno', 15);
 function Dom(seletor) {
     const elementList = document.querySelectorAll(seletor);
     this.elements = elementList;
-    this.addClass = function
+    this.addClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.add(classe);
+        });
+    };
+    this.removeClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.remove(classe);
+        });
+    };
 };
+
+const listaItens = new Dom('li');
+listaItens.addClass('ativar');
