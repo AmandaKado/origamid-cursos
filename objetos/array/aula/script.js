4a80ede63bb97f608ad54ab68ea5f07f7a46b9d9
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-05 10:47:28 -0300

    Fazendo aula sobre arrays

---
+++ objetos/array/aula/script.js
@@ -0,0 +1,75 @@
+// const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+// const precos = [49, 99, 69, 89];
+
+// const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+
+// console.log(instrumentos[0]);
+// console.log(precos[2]);
+// console.log(dados[1]);
+// console.log(dados[1][0]);
+// console.log(dados[1][1]);
+// console.log(dados[1][2]);
+// console.log(dados[1][2].cor);
+// console.log(dados[0].toLowerCase());
+// console.log(dados[2] ('eu mesmo'));
+
+// const carros = new Array ('Ford', 'Fiat', 'Honda');
+
+// carros[0] = 'Ferrari';
+// carros[1] = 'Kia';
+// carros[2] = 'Fusca';
+// carros[20] = 'Kombi';
+
+// console.log(carros);
+// console.log(carros.length);
+
+// const li = document.querySelectorAll('li');
+// const arrayLi = Array.from(li);
+// const obj = {
+//     0: 'Andre',
+//     1: 'Rafael',
+//     2: 'teste',
+//     length: 3,
+// };
+// const arrayObj = Array.from(obj);
+
+// console.log(li);
+// console.log(arrayLi);
+// console.log(arrayObj);
+
+// console.log(Array.isArray(li));
+// console.log(Array.isArray(arrayLi));
+
+// console.log(Array.of('palavra 0', 'palavra 1'));
+// console.log(Array(20));
+
+// const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+
+// console.log(frutas.length);
+// console.log(frutas[0].length);
+// console.log(frutas[1].length);
+// console.log(frutas[2].length);
+// console.log(frutas[2][0].length);
+// console.log(frutas[2][1].length);
+
+// const instrumentos2 = ['Guitarra', 'Baixo', 'Violão'];
+
+// const idades = [1, 34, 22, 30, 23, 443, 54]
+
+// console.log(instrumentos2);
+// console.log(instrumentos2.sort());
+// console.log(instrumentos2);
+
+// console.log(idades);
+// console.log(idades.sort());
+// console.log(idades);
+
+const carros = ['Ford', 'Fiat', 'VW'];
+carros.unshift('Kia', 'Ferrari');
+carros.push('Parati', 'Gol');
+console.log(carros.pop());
+console.log(carros.shift());
+carros.reverse();
+
+console.log(carros);
+
