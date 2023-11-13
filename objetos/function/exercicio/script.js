14e81330c3198f11633374b27d909f681bd1880b
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-13 19:24:36 -0300

    fazendo

--- objetos/function/exercicio/script.js
+++ objetos/function/exercicio/script.js
@@ -10,7 +10,37 @@ console.log(totalLetras);
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+    const elemento = document.createElement(tag);
+    if (classe) {
+        elemento.classList.add('classeEscolhida');
+    };
+    if (conteudo) {
+        elemento.textContent = conteudo;
+    };
+    return elemento;
+};
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+function outroElemento(conteudo) {
+    const elemento = document.createElement('h1');
+
+    const addClasse = elemento.classList.add('titulo');
+
+    if (conteudo) {
+        elemento.textContent = conteudo;
+    };
+
+    return elemento;
+}
+
+let texto = outroElemento('Eu adoro batata')
+
+document.body.appendChild(texto);
+
+const novoElemento = criarElemento('p', 'destaque', 'Este é o meu novo elemento');
+document.body.appendChild(novoElemento);
+
