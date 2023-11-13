ec35f99f766f0eb571be635a1c9132cd6c7d4fcb
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-13 13:11:45 -0300

    fazendo exercicio

--- objetos/function/exercicio/script.js
+++ objetos/function/exercicio/script.js
@@ -1,40 +1,16 @@
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
-const cursos = document.getElementsByClassName('curso');
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
 
-cursos = Array.from(cursos);
+const totalLetras = Array.from(paragrafos).reduce((acumulador, paragrafo) => {
+    return acumulador + paragrafo.textContent.length;
+}, 0);
 
-// Retorne uma lista com os
-// números maiores que 100
-const numeros = [3, 44, 333, 23, 122, 322, 33];
+console.log(totalLetras);
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
 
-
-// Verifique se Baixo faz parte
-// da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-
-
-// Retorne o valor total das compras
-const compras = [
-    {
-        item: 'Banana',
-        preco: 'R$ 4,99'
-    },
-    {
-        item: 'Ovo',
-        preco: 'R$ 2,99'
-    },
-    {
-        item: 'Carne',
-        preco: 'R$ 25,49'
-    },
-    {
-        item: 'Refrigerante',
-        preco: 'R$ 5,35'
-    },
-    {
-        item: 'Quejo',
-        preco: 'R$ 10,60'
-    }
-];
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
