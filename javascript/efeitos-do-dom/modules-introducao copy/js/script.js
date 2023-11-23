8917e3b1390bddf938edcdcb10b9fed31a1436e1
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-23 20:15:45 -0300

    ajeitando pasta para proxima aula

---
+++ javascript/efeitos-do-dom/modules-introducao copy/js/script.js
@@ -0,0 +1,10 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+
+console.log(testes.senha);
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
