5061ad3df9bd3383e8d1004b9d7e9717f0065188
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-23 20:07:55 -0300

    fazendo segunda aula de modules

---
+++ javascript/efeitos-do-dom/modules-introducao/js/modules/testes.js
@@ -0,0 +1,9 @@
+export function teste1() {
+    console.log('é o teste 1');
+};
+
+export function teste2() {
+    console.log('é o teste 2');
+};
+
+export const senha = 2312312312;
--- javascript/efeitos-do-dom/modules-introducao/js/script.js
+++ javascript/efeitos-do-dom/modules-introducao/js/script.js
@@ -2,7 +2,13 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+// import { teste1, teste2 } from "./modules/testes.js";
+import * as testes from "./modules/testes.js"
 
+
+testes.teste1();
+testes.teste2();
+console.log(testes.senha);
 initTabNav();
 initAccordion();
 initScrollSuave();
