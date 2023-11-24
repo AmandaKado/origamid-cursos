2d3badc63c73b59df311f5b7a4856e0de3da4ec6
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-24 13:17:02 -0300

    arrumando pastas css

--- javascript/efeitos-do-dom/modais/js/script.js
+++ javascript/efeitos-do-dom/modais/js/script.js
@@ -3,7 +3,6 @@ import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 
-console.log(testes.senha);
 initTabNav();
 initAccordion();
 initScrollSuave();
