ccf17c8ade8dd00ccb039e616d09016a9d051f5a
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-11-24 14:40:11 -0300

    aula sobre modal

--- javascript/efeitos-do-dom/modais/js/script.js
+++ javascript/efeitos-do-dom/modais/js/script.js
@@ -2,7 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
 
+
+initModal();
 initTabNav();
 initAccordion();
 initScrollSuave();
