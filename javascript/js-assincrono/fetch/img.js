1a76481a14607c9a31bd6540e11699c8bdbef890
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2024-01-19 19:51:19 -0300

    aula 2 de fetch

---
+++ javascript/js-assincrono/fetch/img.js
@@ -0,0 +1,8 @@
+const imagem = fetch('./img.png');
+
+imagem.then(r => r.blob())
+.then(body => {
+    const blobUrl = URL.createObjectURL(body);
+    const imagemDom = document.querySelector('img');   
+    imagemDom.src = blobUrl;
+})
--- javascript/js-assincrono/fetch/script.js
+++ javascript/js-assincrono/fetch/script.js
@@ -1,8 +1,37 @@
 const cep = fetch('https://viacep.com.br/ws/88220000/json');
 
-cep.then(resolucao => resolucao.json()) 
-    .then(body => {
-        console.log(body.cep);
-        const conteudo = document.querySelector('.conteudo');
-        conteudo.innerTexto = body.cep;
+// cep.then(resolucao => resolucao.json()) 
+//     .then(body => {
+//         console.log(body.cep);
+//         const conteudo = document.querySelector('.conteudo');
+//         conteudo.innerTexto = body.cep;
+//     });
+
+cep.then(r => {
+    const r2= r.clone()
+    r.text().then((text) => {
+        console.log(text);
     });
+    r2.json().then((json) => {
+        console.log(json);
+    });
+    console.log(r);
+})
+.then(body => {
+    console.log(body);
+});
+
+const array = ['item 1', 'item 2' ,'item 3'];
+
+array.forEach(console.log);
+
+cep.then(response => {
+    console.log(response.status);
+    console.log(response.url);
+    console.log(response.type);
+    if(response.status === 404) {
+        console.log('Página não existe');
+    } else {
+        console.log('Encontrado');
+    };
+});
